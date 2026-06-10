5438407695ae96cf4a52bfb665b38b02f163f811
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-06-10 19:56:05 +0600

    !

--- spotlight-app-a/app/index.tsx
+++ spotlight-app-a/app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from 'react-native';
+import { Image, Pressable, Text, TouchableOpacity, View } from 'react-native';
+import { styles } from '../styles/auth.styles';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen. O_o</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello!</Text>
+
+      <TouchableOpacity onPress={() => alert('🫣')}>
+        <Text>Press Me</Text>
+      </TouchableOpacity>
+
+      <Pressable onPress={() => alert('You Pressed!')}>
+        <Text>Press Me</Text>
+      </Pressable>
+
+      <Image
+        source={require('../assets/images/splash-icon.png')}
+        style={{ width: 200, height: 200 }}
+      />
+      <Image
+        source={{
+          uri: 'https://images.unsplash.com/photo-1672814519670-16eedb80ac83?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        }}
+        style={{ width: 200, height: 200, resizeMode: 'cover' }}
+      />
     </View>
   );
 }
